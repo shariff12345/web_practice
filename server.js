6a042df5bfe91ac7fbf094a422d353f81796431b
shariff12345 <abirinsharf@gmail.com>
2025-08-02 20:02:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const appName = process.env.APP_NAME
 Arts_filesapp.use('/Peoples of the Philippines_ Tausug - National Commission for Culture and the ', express.static(path.join(__dirname, 'Peoples of the Philippines_ Tausug - National Commission for Culture and the Arts_files')));
 
 app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Peoples of the Philippines_ Tausug - National Commission for Culture and the Arts.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
     console.log(`Request served by ${appName}`);
 });
 
 app.listen(port, () => {
     console.log(`${appName} is listening on port ${port}`);
 });
+
 
